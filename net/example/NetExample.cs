4568fb1c7ec139ba8dc80087efc600b2d45b13de
Heejin Lim <hlim@toss.im>
2022-05-08 01:55:37 +0900

    EncryptSessionAesKey 빼고 모두 작업 완료

--- net/example/NetExample.cs
+++ net/example/NetExample.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using static Functions;
 
 class NetExample
--- net/test/NetExampleTest.cs
+++ net/test/NetExampleTest.cs
@@ -1,89 +1,89 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
 using System.Security.Cryptography;
 using System.Text;
+
 using static TestData;
 using static Functions;
 
-namespace NetExample.Test
+class NetExampleTest
 {
-    [TestClass]
-    public class NetExampleTest
+    static void Main(string[] args)
     {
-        [TestMethod]
-        public void TestDecryptSessionKey()
+        TestDecryptSessionKey();
+        TestGenerateSessionKey();
+        TestEncryptDecryptData();
+    }
+
+    public static void TestDecryptSessionKey()
+    {
+        for (int i = 0; i < RSA_TEST_DATA.GetLength(0); i++)
         {
-            for (int i = 0; i < RSA_TEST_DATA.GetLength(0); i++)
-            {
-                var secretKey = RSA_TEST_DATA[i, 1];
-                var iv = RSA_TEST_DATA[i, 2];
-                var sessionKey = RSA_TEST_DATA[i, 3];
-                var decryptedSessionKey = DecryptSessionKey(TEST_BASE64_PRIVATE_KEY, sessionKey.Split('$')[2]);
+            var secretKey = RSA_TEST_DATA[i, 1];
+            var iv = RSA_TEST_DATA[i, 2];
+            var sessionKey = RSA_TEST_DATA[i, 3];
+            var decryptedSessionKey = DecryptSessionKey(TEST_BASE64_PRIVATE_KEY, sessionKey.Split('$')[2]);
 
-                Console.WriteLine(decryptedSessionKey);
-                AssertEquals("AES_GCM$" + secretKey + "$" + iv, decryptedSessionKey);
-            }
+            Console.WriteLine(decryptedSessionKey);
+            AssertEquals("AES_GCM$" + secretKey + "$" + iv, decryptedSessionKey);
         }
+    }
 
-        [TestMethod]
-        public void TestGenerateSessionKey()
+    public static void TestGenerateSessionKey()
+    {
+        for (int i = 0; i < RSA_TEST_DATA.GetLength(0); i++)
         {
-            for (int i = 0; i < RSA_TEST_DATA.GetLength(0); i++)
-            {
-                var sessionId = RSA_TEST_DATA[i, 0];
-                var secretKey = RSA_TEST_DATA[i, 1];
-                var iv = RSA_TEST_DATA[i, 2];
-                var sessionKey = RSA_TEST_DATA[i, 3];
-                var generatedSessionKey = GenerateSessionKey(sessionId, secretKey, iv, TEST_BASE64_PUBLIC_KEY);
-                var decryptedSessionKey = DecryptSessionKey(TEST_BASE64_PRIVATE_KEY, sessionKey.Split('$')[2]);
+            var sessionId = RSA_TEST_DATA[i, 0];
+            var secretKey = RSA_TEST_DATA[i, 1];
+            var iv = RSA_TEST_DATA[i, 2];
+            var sessionKey = RSA_TEST_DATA[i, 3];
+            var generatedSessionKey = GenerateSessionKey(sessionId, secretKey, iv, TEST_BASE64_PUBLIC_KEY);
+            var decryptedSessionKey = DecryptSessionKey(TEST_BASE64_PRIVATE_KEY, sessionKey.Split('$')[2]);
 
-                Console.WriteLine(generatedSessionKey);
-                Console.WriteLine(decryptedSessionKey);
+            Console.WriteLine(generatedSessionKey);
+            Console.WriteLine(decryptedSessionKey);
 
-                AssertEquals(sessionKey.Substring(0, 40), generatedSessionKey.Substring(0, 40));
-                AssertEquals("AES_GCM$" + secretKey + "$" + iv, decryptedSessionKey);
-            }
+            AssertEquals(sessionKey.Substring(0, 40), generatedSessionKey.Substring(0, 40));
+            AssertEquals("AES_GCM$" + secretKey + "$" + iv, decryptedSessionKey);
         }
+    }
 
-        [TestMethod]
-        public void TestEncryptDecryptData()
+    public static void TestEncryptDecryptData()
+    {
+        for (int i = 0; i < AES_TEST_DATA.GetLength(0); i++)
         {
-            for (int i = 0; i < AES_TEST_DATA.GetLength(0); i++)
-            {
-                var sessionId = AES_TEST_DATA[i, 0];
-                var secretKey = AES_TEST_DATA[i, 1];
-                var iv = AES_TEST_DATA[i, 2];
-                var plain = AES_TEST_DATA[i, 3];
-                var encrypted = AES_TEST_DATA[i, 4];
+            var sessionId = AES_TEST_DATA[i, 0];
+            var secretKey = AES_TEST_DATA[i, 1];
+            var iv = AES_TEST_DATA[i, 2];
+            var plain = AES_TEST_DATA[i, 3];
+            var encrypted = AES_TEST_DATA[i, 4];
 
-                String encryptedData = EncryptData(sessionId, secretKey, iv, plain);
-                String decryptedData = DecryptData(secretKey, iv, encrypted);
+            String encryptedData = EncryptData(sessionId, secretKey, iv, plain);
+            String decryptedData = DecryptData(secretKey, iv, encrypted);
 
-                Console.WriteLine(encryptedData);
-                Console.WriteLine(decryptedData);
+            Console.WriteLine(encryptedData);
+            Console.WriteLine(decryptedData);
 
-                AssertEquals(encrypted, encryptedData);
-                AssertEquals(plain, decryptedData);
-            }
+            AssertEquals(encrypted, encryptedData);
+            AssertEquals(plain, decryptedData);
         }
+    }
 
-        public static void AssertEquals(string expected, string actual)
+    public static void AssertEquals(string expected, string actual)
+    {
+        if (expected != actual)
         {
-            if (expected != actual)
-            {
-                Console.Error.WriteLine("Expected: " + expected);
-                Console.Error.WriteLine("Actual: " + actual);
-                throw new SystemException("Assertion failed");
-            }
+            Console.Error.WriteLine("Expected: " + expected);
+            Console.Error.WriteLine("Actual: " + actual);
+            throw new SystemException("Assertion failed");
         }
+    }
 
-        public static string DecryptSessionKey(string base64PrivateKey, string sessionKey)
-        {
-            var rsaCipher = new RSACryptoServiceProvider();
-            rsaCipher.ImportPkcs8PrivateKey(Convert.FromBase64String(base64PrivateKey), out _);
+    public static string DecryptSessionKey(string base64PrivateKey, string sessionKey)
+    {
+        var rsaCipher = new RSACryptoServiceProvider();
+        rsaCipher.ImportPkcs8PrivateKey(Convert.FromBase64String(base64PrivateKey), out _);
 
-            byte[] decrypted = rsaCipher.Decrypt(Convert.FromBase64String(sessionKey), RSAEncryptionPadding.OaepSHA1);
-            return Encoding.UTF8.GetString(decrypted);
-        }
+        byte[] decrypted = rsaCipher.Decrypt(Convert.FromBase64String(sessionKey), RSAEncryptionPadding.OaepSHA1);
+        return Encoding.UTF8.GetString(decrypted);
     }
 }
