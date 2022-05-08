5d60026f76d46110a25a3ee6254fcd6b31f4bbae
Heejin Lim <hlim@toss.im>
2022-05-08 19:42:21 +0900

    Ruby 코드 예시 개발 완료

---
+++ ruby/src/functions.rb
@@ -0,0 +1,47 @@
+require 'base64'
+require 'openssl'
+
+def generate_random_bytes(length_in_bits)
+  Base64.strict_encode64(SecureRandom.random_bytes(length_in_bits / 8))
+end
+
+def generate_session_key(session_id, secret_key, iv, base64_public_key)
+  session_aes_key = 'AES_GCM$' + secret_key + '$' + iv
+  encrypted_session_aes_key = encrypt_session_aes_key(base64_public_key, session_aes_key)
+  'v1$' + session_id + '$' + encrypted_session_aes_key
+end
+
+def encrypt_session_aes_key(base64_public_key, session_aes_key)
+  public_key = OpenSSL::PKey::RSA.new(Base64.strict_decode64(base64_public_key))
+
+  encrypted = public_key.public_encrypt(session_aes_key, OpenSSL::PKey::RSA::PKCS1_OAEP_PADDING)
+  Base64.strict_encode64(encrypted)
+end
+
+def encrypt_data(session_id, secret_key, iv, data)
+  cipher = OpenSSL::Cipher.new('aes-256-gcm')
+  cipher.encrypt
+  cipher.key = Base64.strict_decode64(secret_key)
+  cipher.iv = Base64.strict_decode64(iv)
+  cipher.auth_data = Base64.strict_decode64(secret_key)
+
+  encrypted = cipher.update(data) + cipher.final
+  combined = Base64.strict_encode64(encrypted + cipher.auth_tag)
+  'v1$' + session_id + '$' + combined
+end
+
+def decrypt_data(secret_key, iv, encrypted_data)
+  parsed = Base64.strict_decode64(encrypted_data.split('$')[2])
+  encrypted = parsed[0..-17]
+  tag = parsed[-16..-1]
+
+  cipher = OpenSSL::Cipher.new('aes-256-gcm')
+  cipher.decrypt
+  cipher.key = Base64.strict_decode64(secret_key)
+  cipher.iv = Base64.strict_decode64(iv)
+  cipher.auth_data = Base64.strict_decode64(secret_key)
+  cipher.auth_tag = tag
+
+  decrypted = cipher.update(encrypted) + cipher.final
+  decrypted.force_encoding('UTF-8')
+end
---
+++ ruby/test/ruby_example_test.rb
@@ -0,0 +1,5 @@
+require_relative 'test_functions'
+
+test_decrypt_session_key
+test_generate_session_key
+test_encrypt_decrypt_data
